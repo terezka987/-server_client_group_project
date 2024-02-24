bb44a94a8a46b78a19c23f55da61a5aa498f4c78
Peter Clark <sgpclar2@liverpool.ac.uk>
2024-02-24 16:36:03 +0000

    Client wasnt exiting on stop

--- DemoEchoServer/client.py
+++ DemoEchoServer/client.py
@@ -8,6 +8,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         user_input = input("Enter your message \n")
         s.sendall(str(user_input).encode('utf-8'))
+        if user_input == "stop":
+            break
+        
         data = s.recv(1024)
-
+        s
         print('Echoing: ', repr(data.decode()))
