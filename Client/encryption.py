8ca1b048a6e7acbb63b89597c87dbaa7bf76267f
Peter Clark <sgpclar2@liverpool.ac.uk>
2024-03-08 21:28:38 +0000

    Add encryption class

--- Client/encryption.py
+++ Client/encryption.py
@@ -5,3 +5,78 @@ from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 import secrets
 import base64
 import getpass
+
+
+class KeyHolder:
+    """Is initialised with password and generates a key which is held as a member"""
+
+    def __init__(self, password: str):
+        self.__salt = b'0'
+        self.__key = b'0'
+        self.__generate_key(password)
+
+    def __generate_salt(self, size=16):
+        """Set salt member"""
+        self.__salt = secrets.token_bytes(size)
+
+    def __derive_key(self, salt: bytes, password: str) -> bytes:
+        """Derive the key from the `password` using the passed `salt`"""
+        key = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
+        return key.derive(password.encode())
+
+    def __generate_key(self, password: str):
+        """
+        sets self.__key from a `password` and the salt.
+        """
+        salt_size = 16
+
+        # generate new salt and save it
+        self.__generate_salt(salt_size)
+        # generate the key from the salt and the password
+        self.__derive_key(self.__salt, password)
+
+        # encode it using Base 64 and return it
+        self.__key = base64.urlsafe_b64encode(self.__key)
+        print(self.__key)
+        print(self.__salt)
+
+    def get_salt(self) -> bytes:
+        """Return salt"""
+        # print
+        return self.__salt
+
+    def get_key(self) -> bytes:
+        """Return key"""
+        return self.__key
+
+    # def encrypt(contents_to_encrypt: str, filename: str, key: str):
+    #     """
+    #     Takes some contents, encrypts using key and creates filename in current directory
+    #     contents_to_encrypt: the string to be encrypted
+    #     filename: filename to create with encrypted contents
+    #     key: key to use for for encryption/decryption
+    #     """
+    #     fernet = Fernet(key)
+    #     encrypted_data = fernet.encrypt(contents_to_encrypt)
+
+    #     with open(filename, "wb") as file:
+    #         file.write(encrypted_data)
+
+    # def decrypt(filename, key):
+    #     """
+    #     Given a filename (str) and key (bytes), it decrypts the file and write it
+    #     """
+    #     f = Fernet(key)
+    #     with open(filename, "rb") as file:
+    #         # read the encrypted data
+    #         encrypted_data = file.read()
+    #     # decrypt data
+    #     try:
+    #         decrypted_data = f.decrypt(encrypted_data)
+    #     except cryptography.fernet.InvalidToken:
+    #         print("Invalid token, most likely the password is incorrect")
+    #         return
+    #     # write the original file
+    #     with open(filename, "wb") as file:
+    #         file.write(decrypted_data)
+    #     print("File decrypted successfully")
