e8bce8dec2cd628da7fd4313b788dad33d9a774c
Tereza Gonsiorova <sgtgonsi@liverpool.ac.uk>
2024-03-13 10:17:22 +0100

    getting tests to work after rebase

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -11,8 +11,8 @@ print(sys.path)
 
 from unittest.mock import AsyncMock, MagicMock
 
-from Client import client
-from Server import server
+from Client.client import Client
+from Server.server import Server
 from Common import handleuserinput
 
 class Unittests (unittest.TestCase):
@@ -23,32 +23,35 @@ class Unittests (unittest.TestCase):
 
     def test_dictionary_bytes(self):
         expected_bytes = b'\x80\x04\x95S\x00\x00\x00\x00\x00\x00\x00}\x94\x8c\x04boat\x94}\x94(\x8c\x04size\x94\x8c\x04ship\x94\x8c\x07country\x94\x8c\x07Finland\x94\x8c\x05cargo\x94\x8c\x08tomatoes\x94\x8c\x05color\x94\x8c\x04pink\x94us.'
-        bytes = client.create_dictionary_bytes()
+        client = Client()
+        bytes = client._create_dictionary_bytes()
         self.assertEqual(bytes, expected_bytes)
         
     def test_dictionary_json(self):
         expected_json = b'{\n  "boat": {\n    "size": "ship",\n    "country": "Finland",\n    "cargo": "tomatoes",\n    "color": "pink"\n  }\n}'
-        json = client.create_dictionary_json()
+        client = Client()
+        json = client._create_dictionary_json()
         self.assertEqual(json, expected_json)
    
 
     def test_dictionary_xml(self):
         expected_xml = b'<?xml version="1.0" encoding="utf-8"?>\n<boat>\n\t<size>ship</size>\n\t<country>Finland</country>\n\t<cargo>tomatoes</cargo>\n\t<color>pink</color>\n</boat>'
-        xml = client.create_dictionary_xml()
+        client = Client()
+        xml = client._create_dictionary_xml()
         self.assertEqual(xml, expected_xml)
 
 
 # Testing validating user input as integer within range 0 to 5
         
     def test_handle_user_input(self):
-        self.assertEqual(handleuserinput.handle_top_level_input(1),1)
-        self.assertEqual(handleuserinput.handle_top_level_input(0),0)
-        self.assertEqual(handleuserinput.handle_top_level_input(2),2)
-        self.assertEqual(handleuserinput.handle_top_level_input(3),3)
-        self.assertEqual(handleuserinput.handle_top_level_input(4),4)
-        self.assertEqual(handleuserinput.handle_top_level_input(5),5)
-        self.assertEqual(handleuserinput.handle_top_level_input(6),-1)
-        self.assertEqual(handleuserinput.handle_top_level_input("Tereza"),-1)
+        self.assertEqual(handleuserinput.handle_client_options(1),1)
+        self.assertEqual(handleuserinput.handle_client_options(0),0)
+        self.assertEqual(handleuserinput.handle_client_options(2),2)
+        self.assertEqual(handleuserinput.handle_client_options(3),3)
+        self.assertEqual(handleuserinput.handle_client_options(4),4)
+        self.assertEqual(handleuserinput.handle_client_options(5),5)
+        self.assertEqual(handleuserinput.handle_client_options(6),-1)
+        self.assertEqual(handleuserinput.handle_client_options("Tereza"),-1)
 
 class Unittests_async (unittest.IsolatedAsyncioTestCase):
 
@@ -68,9 +71,9 @@ class Unittests_async (unittest.IsolatedAsyncioTestCase):
                          'cargo': 'tomatoes', 'color': 'pink'}}
         data_bytes = pickle.dumps(DICT_TO_SEND)
 
-  
+        server = Server()
         reader.read = AsyncMock(return_value=data_json)
-        await server.receive_message(reader, writer)
+        await server._receive_message(reader, writer)
 
         # Asserts
         writer.get_extra_info.assert_called_once_with('peername')
@@ -93,8 +96,9 @@ class Unittests_async (unittest.IsolatedAsyncioTestCase):
                          'cargo': 'tomatoes', 'color': 'pink'}}
         
 
+        server = Server()
         reader.read = AsyncMock(return_value=data_xml)
-        await server.receive_message(reader, writer)
+        await server._receive_message(reader, writer)
 
         # # Asserts
         writer.get_extra_info.assert_called_with('peername')
@@ -118,7 +122,8 @@ class Unittests_async (unittest.IsolatedAsyncioTestCase):
                          'cargo': 'tomatoes', 'color': 'pink'}}
         data_bytes = pickle.dumps(DICT_TO_SEND)
         reader.read = AsyncMock(return_value=data_bytes)
-        await server.receive_message(reader, writer)
+        server = Server()
+        await server._receive_message(reader, writer)
 
         # # Asserts
         writer.get_extra_info.assert_called_with('peername')
