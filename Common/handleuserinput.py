4d0be9e621e61ee0950cd34e556db929ae40ce5b
Peter Clark <sgpclar2@liverpool.ac.uk>
2024-03-16 11:40:05 +0000

    Remove now unused function, print to screen or function done via command line

--- Common/handleuserinput.py
+++ Common/handleuserinput.py
@@ -36,22 +36,6 @@ def handle_whether_to_encrypt(user_input: str) -> tuple:
         return (True, False)
 
 
-def handle_whether_to_print_or_create_file(user_input: str) -> str:
-    """
-    The server can create text files or print out contents, user selects either
-    First return value is whether valid input was provided
-    Second return value is what user selected
-    """
-    if user_input not in ('file', 'screen'):
-        print("Entered value is not 'file' or 'screen', please enter one of these values")
-        return (False, False)
-
-    if user_input == 'file':
-        return (True, 'file')
-    else:
-        return (True, 'screen')
-
-
 def get_password_from_user(set: bool) -> str:
     """
     Prompt the user for a password
